f80d09e1349efd1de9111fdf5125d975fd94663d
MelissaIuga <144784866+MelissaIuga@users.noreply.github.com>
2025-05-17 17:12:44 +0200

    deployment config

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/hikeway-webapp/"}>
+        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/cue-assets/"}>
             <App />
         </BrowserRouter>
     </React.StrictMode>
