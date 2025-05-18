f3c51c7b2f656f6dc57aede6a762f16679b958cd
MelissaIuga <144784866+MelissaIuga@users.noreply.github.com>
2025-05-18 15:21:39 +0200

    h

--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -13,7 +13,15 @@ export default function ContainerCard({ container }) {
       <div className="inner-collection-card">
         <h3 style={{ fontSize: "12px" }}>{container.title}</h3>
 
+        <h2>{container.assets} New Assets Added</h2>
 
+        <section style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+       
+       <div style={{ display: "flex", alignItems: "center"}}>
+         <UserAvatar uid={container.ownerImage} size={24} />
+         <p style={{ margin: 0 }}>{container.owner}</p>
+       </div>
+       <p style={{ marginLeft: 43 }}>{container.edited}</p>
 
 
 
