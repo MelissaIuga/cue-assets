c791676cda093b44a4426ae1e1857e40776a555c
MelissaIuga <144784866+MelissaIuga@users.noreply.github.com>
2025-05-18 15:20:33 +0200

    h

--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -14,18 +14,7 @@ export default function ContainerCard({ container }) {
         <h3 style={{ fontSize: "12px" }}>{container.title}</h3>
 
 
-        <h2>{container.assets} New Assets Added</h2>
 
-
-        <section style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-  {/* User Avatar and Name */}
-  <div style={{ display: "flex", alignItems: "center"}}>
-    <UserAvatar uid={container.ownerImage} size={24} />
-    <p style={{ margin: 0 }}>{container.owner}</p>
-  </div>
-
-  {/* Edited Text */}
-  <p style={{ marginLeft: 43 }}>{container.edited}</p>
 
 
 
