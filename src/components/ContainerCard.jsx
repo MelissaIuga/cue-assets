9bf92022a0fc8500241c4294b9c4742fb606180a
MelissaIuga <144784866+MelissaIuga@users.noreply.github.com>
2025-05-18 15:12:05 +0200

    containers

--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -3,24 +3,29 @@ import UserAvatar from "./UserAvatar";
 export default function ContainerCard({ container }) {
   return (
     <article className="collection-card">
-<img
-  src={container.image}
-  alt={container.title}
-  style={{ height: "78px", alignSelf: "stretch", objectFit: "cover" }}
-/>
+      <img
+        src={container.image}
+        alt={container.title}
+        style={{ height: "78px", alignSelf: "stretch", objectFit: "cover" }}
+      />
 
-      
+
       <div className="inner-collection-card">
-      <h3 style={{ fontSize:"12px" }}>{container.title}</h3>
+        <h3 style={{ fontSize: "12px" }}>{container.title}</h3>
 
 
-      <h2>{container.assets} New Assets Added</h2>
+        <h2>{container.assets} New Assets Added</h2>
 
+        <section style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+  {/* User Avatar and Name */}
+  <div style={{ display: "flex", alignItems: "center"}}>
+    <UserAvatar uid={container.ownerImage} size={24} />
+    <p style={{ margin: 0 }}>{container.owner}</p>
+  </div>
 
-      <div style={{ display: "flex", alignItems: "center", gap: "8px", marginTop: "0.5em" }}>
-        <UserAvatar uid={container.owner} size={24} />
-      </div>
-      <p>{container.edited}</p>
+  {/* Edited Text */}
+  <p style={{ marginLeft: 43 }}>{container.edited}</p>
+</section>
       </div>
     </article>
   );
